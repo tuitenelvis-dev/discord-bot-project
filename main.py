cdab1f535ead06a4331301d332e56a01a21521c5
tuitenelvis-dev <elvisnguyenquang2009@gmail.com>
2026-02-24 22:00:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -737,4 +737,21 @@ async def report(ctx, *, content: str):
 # =========================
 # RUN BOT
 # =========================
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+keep_alive()
 bot.run(TOKEN)
